7ef1fa4eace05142845320a59575f58a09fd862d
Nathan Caza <mastercactapus@gmail.com>
2015-12-05 14:16:19 -0600

    improve help output and add verbose mode

--- main.go
+++ main.go
@@ -47,7 +47,17 @@ type Client struct {
 }
 
 var (
-	mainCmd = &cobra.Command{}
+	mainCmd = &cobra.Command{
+		Use:   "letsencrypt-getcert",
+		Short: "Simple utility for generating signed TLS certificates.",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			v, _ := cmd.Flags().GetBool("verbose")
+			if v {
+				log.SetLevel(log.DebugLevel)
+				log.Debugln("verbose mode enabled")
+			}
+		},
+	}
 
 	genCmd = &cobra.Command{
 		Use:   "generate <domains...>",
@@ -63,13 +73,14 @@ var (
 )
 
 func init() {
+	cobra.EnablePrefixMatching = true
 	mainCmd.PersistentFlags().StringP("bind", "b", ":80", "Bind address. The binding address:port for the server. Note, port 80 on the domain(s) must be mapped to this address.")
 	mainCmd.PersistentFlags().StringP("acme-url", "u", acmeURL, "ACME URL. URL to the ACME directory to use.")
 	mainCmd.PersistentFlags().StringP("output-dir", "d", ".", "Output directory. Certificates and keys will be stored here.")
 	mainCmd.PersistentFlags().StringP("account-key", "k", "acme.key", "ACME account key (PEM format). The account key to use with this CA. If it doesn't exist, one will be generated.")
 	mainCmd.PersistentFlags().Int("bits", 4096, "Bits for RSA key generation.")
 	mainCmd.PersistentFlags().Bool("chain", false, "Include full chain. If set, download and include all LE certificates in the chain.")
-	// log.SetLevel(log.DebugLevel)
+	mainCmd.PersistentFlags().BoolP("verbose", "v", false, "Verbose mode. Logs extra messages for debugging.")
 }
 
 func getChain() []byte {
